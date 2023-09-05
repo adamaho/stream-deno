ca200f0f5cd271bdd7d03853c066d360190d546a
adamaho <aho.adam01@gmail.com>
2023-09-05 00:30:36 -0400

    (feat): adding realtime and connecting to deno jv

---
+++ src/main.ts
@@ -0,0 +1,37 @@
+import { Hono } from "https://deno.land/x/hono@v3.5.6/mod.ts";
+import { createRealtime } from "./realtime.ts";
+
+const app = new Hono();
+
+const INITIAL_COUNT = 0;
+
+const kv = await Deno.openKv();
+await kv.set(["count"], INITIAL_COUNT);
+
+const realtime = createRealtime("count", INITIAL_COUNT);
+
+app.get("/", async (c) => {
+  const count = await kv.get(["count"]);
+  return realtime.response(c.req.raw, count.value ?? INITIAL_COUNT);
+});
+
+app.get("/increment", async () => {
+  const count = await kv.get<number>(["count"]);
+
+  if (typeof count.value === "number") {
+    await kv.set(["count"], count.value + 1);
+    realtime.patch(count.value);
+    return new Response("dene");
+  }
+
+  return new Response("hmm... didnt work bed.");
+});
+
+Deno.serve(
+  {
+    port: 3000,
+    cert: Deno.readTextFileSync("./cert.pem"),
+    key: Deno.readTextFileSync("./key.pem"),
+  },
+  app.fetch
+);
---
+++ src/realtime.ts
@@ -0,0 +1,71 @@
+import jsonpatch from "npm:fast-json-patch";
+
+type RealtimeResponse<T> = {
+  data: T;
+};
+
+/**
+ * Formats the data into the desired response form
+ *
+ * @param d Data to format
+ * @returns An object of data
+ */
+function response(d: unknown) {
+  return { data: d };
+}
+
+/**
+ * Formats data as json
+ *
+ * @param d Data to format into json
+ * @returns json
+ */
+function json(d: RealtimeResponse<unknown>) {
+  return new TextEncoder().encode(`${JSON.stringify(d)}\n`);
+}
+
+class Realtime<RealtimeData = unknown> {
+  private data: unknown;
+  private topic: string;
+
+  constructor(topic: string, initialData: unknown) {
+    this.data = initialData;
+    this.topic = topic;
+  }
+
+  public response = (_req: Request, initialData: unknown) => {
+    const channel = new BroadcastChannel(this.topic);
+
+    const body = new ReadableStream({
+      start(controller) {
+        controller.enqueue(json(response(initialData)));
+
+        channel.onmessage = (e) => {
+          controller.enqueue(json(response(e.data)));
+        };
+      },
+      cancel() {
+        channel.close();
+      },
+    });
+
+    return new Response(body, {
+      headers: {
+        "content-type": "application/json+ndjsonpatch",
+        "x-content-type-options": "nosniff",
+      },
+    });
+  };
+
+  public patch = (d: RealtimeData) => {
+    const patch = jsonpatch.compare(response(this.data), response(d));
+    const channel = new BroadcastChannel(this.topic);
+    channel.postMessage(patch);
+    channel.close();
+    this.data = d;
+  };
+}
+
+export function createRealtime(topic: string, initialData: unknown) {
+  return new Realtime(topic, initialData);
+}
